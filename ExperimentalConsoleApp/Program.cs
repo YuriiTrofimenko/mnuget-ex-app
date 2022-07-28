faaed6f9c784fbec17787cf041ed01e5a21d2677
YuriiTrofimenko <tyaa@ukr.net>
2022-07-28 16:01:58 +0300

    init, demo1

---
+++ ExperimentalConsoleApp/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Mirtek.ExperimentalLib;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine($"2 + 2 = {Actions.Add(2, 2)}");
